1b653ac5e2c7de3dcc505366d1fbfbd0ad7efc60
romain <rvdemael@ulb.ac.be>
2016-03-30 21:53:39 +0200

    better interface

--- project/FlatTriangle.js
+++ project/FlatTriangle.js
@@ -1,6 +1,6 @@
 function ETD(center,proc) { //flat equilateral trangle
   var WIDTH =  300;
-  var HEIGHT = (math.sqrt(3.0)/2.0)*WIDTH;
+  var HEIGHT = math.ceil((math.sqrt(3.0)/2.0)*WIDTH);
 
   this.p1 = new Point(center.getX() , center.getY() - HEIGHT/2 );
   this.p2 = new Point(center.getX() + WIDTH/2 , center.getY() + HEIGHT/2 );
@@ -81,8 +81,10 @@ function ETD(center,proc) { //flat equilateral trangle
 
   this.draw = function() {
     this.p.background(155);
+    this.p.fill(255,0,0);
     this.p.ellipse(this.p1.getX(),this.p1.getY(),pointSize,pointSize);
     this.p.ellipse(this.p2.getX(),this.p2.getY(),pointSize,pointSize);
+    this.p.fill(100);
     this.p.ellipse(this.p3.getX(),this.p3.getY(),pointSize,pointSize);
     this.p.line(this.p1.getX(), this.p1.getY(), this.p2.getX(), this.p2.getY());
     this.p.line(this.p2.getX(), this.p2.getY(), this.p3.getX(), this.p3.getY());
@@ -93,41 +95,49 @@ function ETD(center,proc) { //flat equilateral trangle
   }
 
    this.drawUnFolded = function() {
-    this.p.background(155);
+
+    //this.p.background(155);
     var polygons = [];
+    //this.p.println("OK 1");
     var polygon = new Polygon(this.p,this.c);
-    var polygon2 = new Polygon(this.p,this.c);
-
-    polygon2.addPoint(this.p1.getX(),this.p1.getY());
-    for(i = 0 ; i < this.move.length; i++) {
-      polygon2.addPoint(  this.move[i].getX(), this.p1.getY() - (this.move[i].getY() - this.p1.getY() )    );
-    }
-
-
-    if(this.move[this.move.length-1].getY() == this.p2.getY()) {
-      var x1 = this.move[this.move.length-1].getX();
-      var x2 = this.p2.getX();
-      polygon2.addPoint( x1 , this.p1.getY() - (this.p2.getY() - this.p1.getY())   );
-      polygon2.addPoint( x2 , this.p1.getY() - (this.p2.getY() - this.p1.getY()) );
-
-    }
-
-
-    //polygon2.addPoint(this.p1.getX(),this.p1.getY());
-    polygon2.reduce(2);
-    polygons.push(polygon2);
-    polygon.addPoint(this.p1.getX(),this.p1.getY());
-    polygon.addPoint(this.p2.getX(),this.p2.getY());
-
-    for(i = this.move.length -1 ; i >= 0; i--) {
-      polygon.addPoint(  this.move[i].getX(), this.p2.getY() + ( this.p2.getY() - this.move[i].getY() )    );
-    }
-
+    //this.p.println("OK 2");
     polygon.addPoint(this.p3.getX(),this.p3.getY());
-    polygon.reduce(2);
-    polygon.setLinkedPolygon(polygon2);
-    polygon.move(0,2);
-    polygon2.setLinkedPolygon(polygon);
+    //this.p.println("OK 3");
+    for(var i=0;i<this.move.length;i++) {
+      polygon.addPoint(this.move[i].getX(), this.move[i].getY() + 2*(this.p3.getY() - this.move[i].getY()) );
+    }
+    polygon.addPoint(this.p2.getX(),this.p2.getY());
+    this.p.println("OK -1");
+    //for(j=this.move.length-1;j>=0;j--) {
+      //this.p.println("OK M");
+      var x = this.p3.getX();
+      //this.p.println("OK M1");
+      var y = this.p3.getY();
+      //this.p.println("OK M2");
+      //droite d passant par P1-P2
+      var a= (this.p1.getY() - this.p2.getY())/(this.p1.getX() - this.p2.getX()) ;
+      var b= this.p2.getY() - a*this.p2.getX();
+      this.p.println(a+" , "+b);
+      this.p.ellipse(this.p1.getX()+20,a*(this.p1.getX()+20)+b,pointSize,pointSize);
+
+      //droite d' perpendiculaire à d passant par p => droite normal
+      var a2=(-1/a),b2= y + x/a;
+      //this.p.ellipse(0,b2,pointSize,pointSize);
+      //this.p.fill(0,255,0);
+      //this.p.ellipse(this.p1.getX()+20,a*(this.p1.getX()+20)+b,pointSize,pointSize);
+      //this.p.fill(100);
+      //this.p.println(a2+" , "+b2);
+
+      //p' = intersection d' et d
+      var x2=(b2-b)/(a-a2);
+      var y2= a2*x2 + b2;
+      //p'' = resultat de la symetrie
+      var x3= x + 2*(x2-x) ,y3=y+ 2*(y2-y);
+      //get symetry of move by axis p1-P2
+      polygon.addPoint(x2, y2 );
+    //}
+    this.p.println("OK ");
+    polygon.addPoint(this.p1.getX(),this.p1.getY());
     polygons.push(polygon);
     return polygons;
 
